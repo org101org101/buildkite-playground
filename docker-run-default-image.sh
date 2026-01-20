351ea5730e491609927b15b9deb34d7865bffa0c
deantheking <117676136+deantheking@users.noreply.github.com>
2026-01-20 04:10:02 +0000

    use anza ci image

--- docker-run-default-image.sh
+++ docker-run-default-image.sh
@@ -4,4 +4,4 @@ set -e
 
 here="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-exec "$here/docker-run.sh" ubuntu:22.04 "$@"
+exec "$here/docker-run.sh" anzaxyz/ci:ubuntu-22.04_rust-1.91.1_nightly-2025-09-14_819eca14 "$@"
